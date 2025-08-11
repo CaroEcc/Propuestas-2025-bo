d8f40dd7b9efb9ff4e8ea80856d984d9c031facc
caroecc <carolinavivianescobarcoca@gmail.com>
2025-08-11 11:41:54 -0400

    4

--- src/data/candidates.js
+++ src/data/candidates.js
@@ -1,15 +1,21 @@
-import andronicoImg from '../assets/candidates/andronico.jpg';
+import APImg from '../assets/candidates/API.jpg';
 import ADNImg from '../assets/candidates/ADN.png';
+import APBSImg from '../assets/candidates/APBSImg.png';
 const candidates = [
     {
       id: 1,
-      bgImage: andronicoImg,
+      bgImage: APImg,
       videoEmbed: "https://youtu.be/JrwWqxCCAHw?si=dTFqyzrgesqgTjgs"
     },
     {
       id: 2,
       bgImage: ADNImg,
       videoEmbed: "https://youtu.be/JrwWqxCCAHw?si=dTFqyzrgesqgTjgs"
+    },
+    {
+      id: 3,
+      bgImage: APBSImg,
+      videoEmbed: "https://youtu.be/wn1Y7p8YOeU"
     },
   ];
   
