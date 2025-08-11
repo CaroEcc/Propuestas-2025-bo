0339b12b5518a72b67d8cc255b58f363ad939a5e
caroecc <carolinavivianescobarcoca@gmail.com>
2025-08-11 11:12:03 -0400

    4

--- src/data/candidates.js
+++ src/data/candidates.js
@@ -1,9 +1,14 @@
 import andronicoImg from '../assets/candidates/andronico.jpg';
-
+import ADNImg from '../assets/candidates/ADN.png';
 const candidates = [
     {
       id: 1,
       bgImage: andronicoImg,
+      videoEmbed: "https://youtu.be/JrwWqxCCAHw?si=dTFqyzrgesqgTjgs"
+    },
+    {
+      id: 2,
+      bgImage: ADNImg,
       videoEmbed: "https://youtu.be/JrwWqxCCAHw?si=dTFqyzrgesqgTjgs"
     },
   ];
