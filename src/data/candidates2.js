7d83d204703fdf19b09c92bf7a4b6b36f020bbfd
CaroEcc <carolinavivianescobarcoca@gmail.com>
2025-08-13 01:19:41 -0400

    #22

--- src/data/candidates2.js
+++ src/data/candidates2.js
@@ -10,6 +10,7 @@ const candidates2 = [
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1H0qg4ktb_4vPJLqPkdn2ffTefpzgSqbc/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1UUDcsSKdRRb6P-8EHmZxOEQ4yvnwxtxD/view' target='_blank'>Haz click para ver documento</a>",
             "Encuentra a los diputados uninominales de tu circunscripción. <a class='text-blue-500' href='https://observablehq.com/@mauforonda/en-que-circunscripcion-me-encuentro' target='_blank'>Haz click para saber en que circunscripción te encuentras</a>",
+            "Programa de gobierno ALIANZA POPULAR. <a class='text-blue-500' href='https://web.oep.org.bo/wp-content/uploads/2025/05/PROGRAMA-DE-GOBIERNO-ALIANZA-POPULAR.pdf' target='_blank'>Haz click para ver documento</a>"
         ],
         style: "top-[4%] left-[0.9%] w-[9.2%] h-[92.6%]"
     },
@@ -21,6 +22,7 @@ const candidates2 = [
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1aOo0gzEF8U5oQeXMJV2opYXPdSBGv6s3/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1X9jeRvRogHKjE7iQJfargn9TCDTqWhbD/view' target='_blank'>Haz click para ver documento</a>",
             "Encuentra a los diputados uninominales de tu circunscripción. <a class='text-blue-500' href='https://observablehq.com/@mauforonda/en-que-circunscripcion-me-encuentro' target='_blank'>Haz click para saber en que circunscripción te encuentras</a>",
+            "Programa de gobierno ADN. <a class='text-blue-500' href='https://web.oep.org.bo/wp-content/uploads/2025/05/PROGRAMA-DE-GOBIERNO-LIBERTAD-Y-PROGRESO-ADN.pdf' target='_blank'>Haz click para ver documento</a>"
         ],
         style: "top-[4%] left-[10.7%] w-[9.2%] h-[92.6%]"
     },
@@ -32,6 +34,7 @@ const candidates2 = [
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1Nva3q5ieEi_Dv94IK43TjbHnnmTSiB9v/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1eXZYD94zhoCWCpc8AoXj5GvXy82ei9RO/view' target='_blank'>Haz click para ver documento</a>",
             "Encuentra a los diputados uninominales de tu circunscripción. <a class='text-blue-500' href='https://observablehq.com/@mauforonda/en-que-circunscripcion-me-encuentro' target='_blank'>Haz click para saber en que circunscripción te encuentras</a>",
+            "Programa de gobierno APB - SUMATE. <a class='text-blue-500' href='https://web.oep.org.bo/wp-content/uploads/2025/05/PROGRAMA-DE-GOBIERNO-APB-SUMATE.pdf' target='_blank'>Haz click para ver documento</a>"
         ],
         style: "top-[4%] left-[20.5%] w-[9.2%] h-[92.6%]"
     },
@@ -43,6 +46,7 @@ const candidates2 = [
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1WVEWSo9589x8u9lirA6mEMBhcFTghSeR/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1TPuZFcEYveifPkaXmJABaITyjO-G48JH/view' target='_blank'>Haz click para ver documento</a>",
             "Encuentra a los diputados uninominales de tu circunscripción. <a class='text-blue-500' href='https://observablehq.com/@mauforonda/en-que-circunscripcion-me-encuentro' target='_blank'>Haz click para saber en que circunscripción te encuentras</a>",
+            "Programa de gobierno Libertad y Democracia. <a class='text-blue-500' href='https://web.oep.org.bo/wp-content/uploads/2025/05/PROGRAMA-DE-GOBIERNO-ALIANZA-LIBERTAD-Y-DEMOCRACIA.pdf' target='_blank'>Haz click para ver documento</a>"
         ],
         style: "top-[4%] left-[40.5%] w-[9.2%] h-[92.6%]"
     },
@@ -54,6 +58,7 @@ const candidates2 = [
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1uM5vFLv_l-i_SVfjpRbHkUgT38-Qws5d/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/139g3dICQFIWh38-UFChF1ORTTxQbRYYy/view' target='_blank'>Haz click para ver documento</a>",
             "Encuentra a los diputados uninominales de tu circunscripción. <a class='text-blue-500' href='https://observablehq.com/@mauforonda/en-que-circunscripcion-me-encuentro' target='_blank'>Haz click para saber en que circunscripción te encuentras</a>",
+            "Programa de gobierno Fuerza del Pueblo. <a class='text-blue-500' href='https://web.oep.org.bo/wp-content/uploads/2025/06/PROGRAMA-DE-GOBIERNO-ALIANZA-LA-FUERZA-DEL-PUEBLO-1.pdf' target='_blank'>Haz click para ver documento</a>"
         ],
         style: "top-[4%] left-[50.5%] w-[9.2%] h-[92.6%]"
     },
@@ -65,6 +70,7 @@ const candidates2 = [
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1udQ5jWtGk_HG5BEjkRvd2sm3o9oSQdXD/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1z9UEbt7YNtMmoA4ZuSPSHPTVbNcpLnCQ/view' target='_blank'>Haz click para ver documento</a>",
             "Encuentra a los diputados uninominales de tu circunscripción. <a class='text-blue-500' href='https://observablehq.com/@mauforonda/en-que-circunscripcion-me-encuentro' target='_blank'>Haz click para saber en que circunscripción te encuentras</a>",
+            "Programa de gobierno MAS-IPSP. <a class='text-blue-500' href='https://web.oep.org.bo/wp-content/uploads/2025/05/PROGRAMA-DE-GOBIERNO-MAS-IPSP.pdf' target='_blank'>Haz click para ver documento</a>"
         ],
         style: "top-[4%] left-[60.2%] w-[9.2%] h-[92.6%]"
     },
@@ -76,6 +82,7 @@ const candidates2 = [
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1oGuO5W0nqI7cGacJnO68B66t__QUb9AB/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1oGuO5W0nqI7cGacJnO68B66t__QUb9AB/view' target='_blank'>Haz click para ver documento</a>",
             "Encuentra a los diputados uninominales de tu circunscripción. <a class='text-blue-500' href='https://observablehq.com/@mauforonda/en-que-circunscripcion-me-encuentro' target='_blank'>Haz click para saber en que circunscripción te encuentras</a>",
+            "Programa de gobierno Unidad Nacional. <a class='text-blue-500' href='https://web.oep.org.bo/wp-content/uploads/2025/05/PROGRAMA-DE-GOBIERNO-ALIANZA-UNIDAD.pdf' target='_blank'>Haz click para ver documento</a>"
         ],
         style: "top-[4%] left-[80.0%] w-[9.2%] h-[92.6%]"
     },
@@ -87,6 +94,7 @@ const candidates2 = [
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1cWprXiEIzPt2546SYGp5iQbWxe_qi-Jo/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1lXVQsKmIhCb8HrxgvD296bRCrN4u2VdU/view' target='_blank'>Haz click para ver documento</a>",
             "Encuentra a los diputados uninominales de tu circunscripción. <a class='text-blue-500' href='https://observablehq.com/@mauforonda/en-que-circunscripcion-me-encuentro' target='_blank'>Haz click para saber en que circunscripción te encuentras</a>",
+            "Programa de gobierno del PDC. <a class='text-blue-500' href='https://web.oep.org.bo/wp-content/uploads/2025/05/PROGRAMA-DE-GOBIERNO-PARTIDO-DEMOCRATA-CRISTIANO.pdf' target='_blank'>Haz click para ver documento</a>"
         ],
         style: "top-[4%] left-[90.0%] w-[9.2%] h-[92.6%]"
     },
