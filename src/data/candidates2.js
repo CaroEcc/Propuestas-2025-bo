2654c49a62d3cbe8668b197c31a5c0ed08dc6070
caroecc <carolinavivianescobarcoca@gmail.com>
2025-08-15 01:51:49 -0400

    4

--- src/data/candidates2.js
+++ src/data/candidates2.js
@@ -5,7 +5,7 @@ const candidates2 = [
     {
         id: 1,
         nombre: "Andronico Rodriguez - AP",
-        video: "https://www.youtube.com/embed/wn1Y7YOeU?si=99oDA-zmJUT5UzF",
+        video: "https://www.youtube.com/embed/8pfXOCy3AH0",
         promesas: [
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1H0qg4ktb_4vPJLqPkdn2ffTefpzgSqbc/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1UUDcsSKdRRb6P-8EHmZxOEQ4yvnwxtxD/view' target='_blank'>Haz click para ver documento</a>",
@@ -17,7 +17,7 @@ const candidates2 = [
     {
         id: 2,
         nombre: "Pavel Aracena - ADN",
-        video: "https://www.youtube.com/embed/wn1Y7p8YO?si=99oDA-zmJUT5UzF",
+        video: "https://www.youtube.com/embed/JReFN1yv7wE",
         promesas: [
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1aOo0gzEF8U5oQeXMJV2opYXPdSBGv6s3/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1X9jeRvRogHKjE7iQJfargn9TCDTqWhbD/view' target='_blank'>Haz click para ver documento</a>",
@@ -41,7 +41,7 @@ const candidates2 = [
     {
         id: 5,
         nombre: "Jorge Tuto Quiroga - Libertad y democracia",
-        video: "https://www.youtube.com/embed/wn1Y7p8YeU?si=99oDA-zmJUT5UzFS",
+        video: "https://www.youtube.com/embed/1ijQ7MOSEdA",
         promesas: [
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1WVEWSo9589x8u9lirA6mEMBhcFTghSeR/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1TPuZFcEYveifPkaXmJABaITyjO-G48JH/view' target='_blank'>Haz click para ver documento</a>",
@@ -53,7 +53,7 @@ const candidates2 = [
     {
         id: 6,
         nombre: "Jhonny Fernandez - Fuerza del pueblo",
-        video: "https://www.youtube.com/embed/wn1Y7p8YeU?si=99oDA-zmJUT5UzFS",
+        video: "https://www.youtube.com/embed/Gw5Kg4jA8Ks",
         promesas: [
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1uM5vFLv_l-i_SVfjpRbHkUgT38-Qws5d/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/139g3dICQFIWh38-UFChF1ORTTxQbRYYy/view' target='_blank'>Haz click para ver documento</a>",
@@ -65,7 +65,7 @@ const candidates2 = [
     {
         id: 7,
         nombre: "Eduardo del castillo - MAS-IPSP",
-        video: "https://www.youtube.com/embed/wn1Y7p8YeU?si=99oDA-zmJUT5UzFS",
+        video: "https://www.youtube.com/embed/XJSuqT_xoCg",
         promesas: [
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1udQ5jWtGk_HG5BEjkRvd2sm3o9oSQdXD/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1z9UEbt7YNtMmoA4ZuSPSHPTVbNcpLnCQ/view' target='_blank'>Haz click para ver documento</a>",
@@ -77,7 +77,7 @@ const candidates2 = [
     {
         id: 9,
         nombre: "Samuel Doria Medina - Unidad Nacional",
-        video: "https://www.youtube.com/embed/wn1Y7p8YeU?si=99oDA-zmJUT5UzFS",
+        video: "https://www.youtube.com/embed/R2BtrOuhBPE",
         promesas: [
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1oGuO5W0nqI7cGacJnO68B66t__QUb9AB/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1oGuO5W0nqI7cGacJnO68B66t__QUb9AB/view' target='_blank'>Haz click para ver documento</a>",
@@ -89,7 +89,7 @@ const candidates2 = [
     {
         id: 10,
         nombre: "Rodrigo Paz - PDC",
-        video: "https://www.youtube.com/embed/wn1Y7p8YeU?si=99oDA-zmJUT5UzFS",
+        video: "https://www.youtube.com/embed/RCBeLgDnJ08",
         promesas: [
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1cWprXiEIzPt2546SYGp5iQbWxe_qi-Jo/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1lXVQsKmIhCb8HrxgvD296bRCrN4u2VdU/view' target='_blank'>Haz click para ver documento</a>",
