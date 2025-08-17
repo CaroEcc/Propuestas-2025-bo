f52561c80705850750c1fc2696f9cf88d2778030
caroecc <carolinavivianescobarcoca@gmail.com>
2025-08-16 22:33:30 -0400

    dev

--- src/data/candidates2.js
+++ src/data/candidates2.js
@@ -7,6 +7,7 @@ const candidates2 = [
         nombre: "Andronico Rodriguez - AP",
         video: "https://www.youtube.com/embed/8pfXOCy3AH0",
         promesas: [
+            "Debate completo: <a class='text-blue-500' href='https://www.youtube.com/watch?v=BzVfx8Hf8hg' target='_blank'>Haz click para ver</a>",
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1H0qg4ktb_4vPJLqPkdn2ffTefpzgSqbc/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1UUDcsSKdRRb6P-8EHmZxOEQ4yvnwxtxD/view' target='_blank'>Haz click para ver documento</a>",
             "Encuentra a los diputados uninominales de tu circunscripción. <a class='text-blue-500' href='https://observablehq.com/@mauforonda/en-que-circunscripcion-me-encuentro' target='_blank'>Haz click para saber en que circunscripción te encuentras</a>",
@@ -19,6 +20,7 @@ const candidates2 = [
         nombre: "Pavel Aracena - ADN",
         video: "https://www.youtube.com/embed/JReFN1yv7wE",
         promesas: [
+            "Debate completo: <a class='text-blue-500' href='https://www.youtube.com/watch?v=BzVfx8Hf8hg' target='_blank'>Haz click para ver</a>",
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1aOo0gzEF8U5oQeXMJV2opYXPdSBGv6s3/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1X9jeRvRogHKjE7iQJfargn9TCDTqWhbD/view' target='_blank'>Haz click para ver documento</a>",
             "Encuentra a los diputados uninominales de tu circunscripción. <a class='text-blue-500' href='https://observablehq.com/@mauforonda/en-que-circunscripcion-me-encuentro' target='_blank'>Haz click para saber en que circunscripción te encuentras</a>",
@@ -29,8 +31,9 @@ const candidates2 = [
     {
         id: 3,
         nombre: "Manfred Reyes Villa - APB - SUMATE",
-        video: "https://www.youtube.com/embed/F3u-vhahDZ4",
+        video: "https://www.youtube.com/embed/hYPRLODplHI",
         promesas: [
+            "Debate completo: <a class='text-blue-500' href='https://www.youtube.com/watch?v=BzVfx8Hf8hg' target='_blank'>Haz click para ver</a>",
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1Nva3q5ieEi_Dv94IK43TjbHnnmTSiB9v/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1eXZYD94zhoCWCpc8AoXj5GvXy82ei9RO/view' target='_blank'>Haz click para ver documento</a>",
             "Encuentra a los diputados uninominales de tu circunscripción. <a class='text-blue-500' href='https://observablehq.com/@mauforonda/en-que-circunscripcion-me-encuentro' target='_blank'>Haz click para saber en que circunscripción te encuentras</a>",
@@ -39,10 +42,22 @@ const candidates2 = [
         style: "top-[4%] left-[20.5%] w-[9.2%] h-[92.6%]"
     },
     {
+        id: 4,
+        nombre: "Entre Delincuentes X ARP The High City",
+        video: "https://www.youtube.com/embed/uFqkO5yaEtY",
+        promesas: [
+            "reflejando la coyuntura de #bolivia en una #cancion ",
+            "escrita por ARP The High City, audio producido por @wilrecords y el beat de #EscobarBeatz",
+            "Con el video trato de que el pueblo boliviano no sea una sociedad sin memoria y recuerde antes de ir a las urnas."
+                ],
+        style: "top-[4%] left-[30.5%] w-[9.2%] h-[92.6%]"
+    },
+    {
         id: 5,
         nombre: "Jorge Tuto Quiroga - Libertad y democracia",
         video: "https://www.youtube.com/embed/1ijQ7MOSEdA",
         promesas: [
+            "Debate completo: <a class='text-blue-500' href='https://www.youtube.com/watch?v=BzVfx8Hf8hg' target='_blank'>Haz click para ver</a>",
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1WVEWSo9589x8u9lirA6mEMBhcFTghSeR/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1TPuZFcEYveifPkaXmJABaITyjO-G48JH/view' target='_blank'>Haz click para ver documento</a>",
             "Encuentra a los diputados uninominales de tu circunscripción. <a class='text-blue-500' href='https://observablehq.com/@mauforonda/en-que-circunscripcion-me-encuentro' target='_blank'>Haz click para saber en que circunscripción te encuentras</a>",
@@ -55,6 +70,7 @@ const candidates2 = [
         nombre: "Jhonny Fernandez - Fuerza del pueblo",
         video: "https://www.youtube.com/embed/Gw5Kg4jA8Ks",
         promesas: [
+            "Debate completo: <a class='text-blue-500' href='https://www.youtube.com/watch?v=BzVfx8Hf8hg' target='_blank'>Haz click para ver</a>",
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1uM5vFLv_l-i_SVfjpRbHkUgT38-Qws5d/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/139g3dICQFIWh38-UFChF1ORTTxQbRYYy/view' target='_blank'>Haz click para ver documento</a>",
             "Encuentra a los diputados uninominales de tu circunscripción. <a class='text-blue-500' href='https://observablehq.com/@mauforonda/en-que-circunscripcion-me-encuentro' target='_blank'>Haz click para saber en que circunscripción te encuentras</a>",
@@ -67,6 +83,7 @@ const candidates2 = [
         nombre: "Eduardo del castillo - MAS-IPSP",
         video: "https://www.youtube.com/embed/XJSuqT_xoCg",
         promesas: [
+            "Debate completo: <a class='text-blue-500' href='https://www.youtube.com/watch?v=BzVfx8Hf8hg' target='_blank'>Haz click para ver</a>",
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1udQ5jWtGk_HG5BEjkRvd2sm3o9oSQdXD/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1z9UEbt7YNtMmoA4ZuSPSHPTVbNcpLnCQ/view' target='_blank'>Haz click para ver documento</a>",
             "Encuentra a los diputados uninominales de tu circunscripción. <a class='text-blue-500' href='https://observablehq.com/@mauforonda/en-que-circunscripcion-me-encuentro' target='_blank'>Haz click para saber en que circunscripción te encuentras</a>",
@@ -75,10 +92,24 @@ const candidates2 = [
         style: "top-[4%] left-[60.2%] w-[9.2%] h-[92.6%]"
     },
     {
+        id: 8,
+        nombre: "Matecito de Coca",
+        video: "https://www.youtube.com/embed/KPDiiwPXX-w",
+        promesas: [
+            "@Matecito de Coca",
+            "Análisis, reflexiones, discusiones y puteadas con sus servidoras Kiara y Andrea.",
+            "Matecito de Coca en TikTok. <a class='text-blue-500' href='https://tiktok.com/@matecito.de.coca' target='_blank'>Haz click</a>",
+            "Matecito de Coca en Instagramm. <a class='text-blue-500' href='https://instagram.com/matecitobolivia?igshid=OGQ5ZDc2ODk2ZA==' target='_blank'>Haz click</a>",
+            "Matecito de Coca en FaceBook. <a class='text-blue-500' href='https://facebook.com/profile.php?id=61554354827192&mibextid=ZbWKwL' target='_blank'>Haz click</a>"
+                ],
+        style: "top-[4%] left-[70.1%] w-[9.2%] h-[92.6%]"
+    },
+    {
         id: 9,
         nombre: "Samuel Doria Medina - Unidad Nacional",
         video: "https://www.youtube.com/embed/R2BtrOuhBPE",
         promesas: [
+            "Debate completo: <a class='text-blue-500' href='https://www.youtube.com/watch?v=BzVfx8Hf8hg' target='_blank'>Haz click para ver</a>",
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1oGuO5W0nqI7cGacJnO68B66t__QUb9AB/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1-6wOoH1Yo9m_ebD-Mht76jnmmmC7Jsgk/view' target='_blank'>Haz click para ver documento</a>",
             "Encuentra a los diputados uninominales de tu circunscripción. <a class='text-blue-500' href='https://observablehq.com/@mauforonda/en-que-circunscripcion-me-encuentro' target='_blank'>Haz click para saber en que circunscripción te encuentras</a>",
@@ -91,6 +122,7 @@ const candidates2 = [
         nombre: "Rodrigo Paz - PDC",
         video: "https://www.youtube.com/embed/RCBeLgDnJ08",
         promesas: [
+            "Debate completo: <a class='text-blue-500' href='https://www.youtube.com/watch?v=BzVfx8Hf8hg' target='_blank'>Haz click para ver</a>",
             "Análisis del plan de gobierno de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1cWprXiEIzPt2546SYGp5iQbWxe_qi-Jo/view' target='_blank'>Haz click para ver documento</a>",
             "Ver red flags resultantes del análisis de Chequea tu voto. <a class='text-blue-500' href='https://drive.google.com/file/d/1lXVQsKmIhCb8HrxgvD296bRCrN4u2VdU/view' target='_blank'>Haz click para ver documento</a>",
             "Encuentra a los diputados uninominales de tu circunscripción. <a class='text-blue-500' href='https://observablehq.com/@mauforonda/en-que-circunscripcion-me-encuentro' target='_blank'>Haz click para saber en que circunscripción te encuentras</a>",
